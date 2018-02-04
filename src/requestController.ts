14cff6e50838e0e4df22169f1557e190ebb49968
wk-j <somnuk.wk@gmail.com>
2018-02-05 00:43:49 +0700

    :sparkles: Send request to Terminal

--- src/requestController.ts
+++ src/requestController.ts
@@ -1,5 +1,7 @@
 import { window, Range } from "vscode";
 import { Selector } from "./selector";
+import * as ncp from "copy-paste";
+import * as vscode from "vscode";
 
 export class RequestController {
     public async run(range: Range) {
@@ -8,12 +10,22 @@ export class RequestController {
             return;
         }
 
-        // Get selected text of selected lines or full document
         let selectedText = new Selector().getSelectedText(editor, range);
         if (!selectedText) {
             return;
         }
 
-        console.log(selectedText);
+        let joined = selectedText.replace(/\n/g, " ").replace(/\r\n/g, " ")
+        this.executeInTerminal(joined);
+    }
+
+    executeInTerminal(text: string) {
+        let editor = vscode.window.activeTextEditor;
+        ncp.copy(text + '\n', function () {
+            vscode.commands.executeCommand("workbench.action.terminal.paste").then(() => {
+                editor.show();
+            });
+            editor.show();
+        });
     }
 }
